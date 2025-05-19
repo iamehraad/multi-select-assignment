04c4416dabc83ca6a2ca0fb47e9b101a2067ddef
Mehrad Alinezhad <iamehraad@gmail.com>
2025-05-19 20:20:02 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div
+      className={`font-onest p-3 pb-0 md:p-6 md:pb-0 h-dvh flex flex-col gap-16 main-background`}
+    >
+      <h1 className={"font-onest text-3xl"}>HI</h1>
+    </div>
+  );
+}
+
+export default App;
