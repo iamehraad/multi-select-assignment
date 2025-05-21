ec35a9171437b7a69e5708e4e66d67149c531437
Mehrad Alinezhad <iamehraad@gmail.com>
2025-05-21 20:39:41 +0200

    Added separate URL file to manage api Urls

--- frontend/src/data/requets/productRequest.ts
+++ frontend/src/data/requets/productRequest.ts
@@ -1,10 +1,12 @@
+import { apiUrls } from "../urls";
+
 export const fetchProductsList = async (): Promise<string[]> => {
-    try {
-        const response = await fetch("http://localhost:8080/api/products")
-        const json = await response.json()
-        return json.data
-    } catch (e) {
-        console.log(e)
-        return Promise.reject(e)
-    }
+  try {
+    const response = await fetch(apiUrls.productsList);
+    const json = await response.json();
+    return json.data;
+  } catch (e) {
+    console.log(e);
+    return Promise.reject(e);
+  }
 };
