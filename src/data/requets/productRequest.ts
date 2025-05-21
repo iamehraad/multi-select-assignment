c4ad4c0591acba7a96a4eae03d3a72f9326749e9
Mehrad Alinezhad <iamehraad@gmail.com>
2025-05-21 08:04:12 +0200

    updated mock request

--- src/data/requets/productRequest.ts
+++ src/data/requets/productRequest.ts
@@ -1,3 +1,15 @@
 export const fetchProductsList = (): string[] => {
-  return ["a", "Aa", "b", "Bba", "CCCGGGG", "bbbbaabb", "ccccc", "ddddd"];
+  return [
+    "a",
+    "Aa",
+    "b",
+    "Bba",
+    "CCCGGGG",
+    "bbbbaabb",
+    "ccccc",
+    "ddddd",
+    "play station",
+    "play station2",
+    "play station3",
+  ];
 };
