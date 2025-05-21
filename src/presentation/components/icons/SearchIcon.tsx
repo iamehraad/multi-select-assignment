7079d3519418da61e6946e1690d370a73000ad52
Mehrad Alinezhad <iamehraad@gmail.com>
2025-05-21 06:42:43 +0200

    enhanced input styles

---
+++ src/presentation/components/icons/SearchIcon.tsx
@@ -0,0 +1,8 @@
+import Search from "../../assets/search.svg?react";
+import type { IconProps } from "../../../domain/types/commonTypes";
+
+const SearchIcon = ({ width = 16, height = 16, fill = "black" }: IconProps) => {
+  return <Search width={width} height={height} fill={fill} />;
+};
+
+export default SearchIcon;
