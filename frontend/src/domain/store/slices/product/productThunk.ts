1df04580dc802e2b3eb829a283d61b7d43a83aeb
Mehrad Alinezhad <iamehraad@gmail.com>
2025-05-22 09:30:31 +0200

    added correct rejection method to redux thunk

--- frontend/src/domain/store/slices/product/productThunk.ts
+++ frontend/src/domain/store/slices/product/productThunk.ts
@@ -1,10 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { fetchProductsList } from "../../../../data/requets/productRequest";
+import { FetchApiError } from "../../../types/commonTypes";
 
 export const fetchProductsAsync = createAsyncThunk(
   "product/fetchProducts",
-  async () => {
-    const response = await fetchProductsList();
-    return response;
-  }
+  async (_, thunkAPI) => {
+    try {
+      return await fetchProductsList();
+    } catch (err) {
+      const { status, message } = err as FetchApiError;
+      return thunkAPI.rejectWithValue({
+        status: status || 500,
+        message: message || "Unexpected error",
+      });
+    }
+  },
 );
