b8d218cb11d08e1a94ec0a415f6284be7ca9f66e
Mehrad Alinezhad <iamehraad@gmail.com>
2025-05-23 23:12:24 +0200

    wrapped search and list component in memo to prevent redundant rendering

--- frontend/src/presentation/components/form/inputs/SearchInput.tsx
+++ frontend/src/presentation/components/form/inputs/SearchInput.tsx
@@ -1,4 +1,5 @@
 import SearchIcon from "../../icons/SearchIcon";
+import { memo } from "react";
 
 interface Props {
   searchQuery: string;
@@ -6,22 +7,24 @@ interface Props {
   setSearchQuery: (query: string) => void;
 }
 
-const SearchInput = ({ searchQuery, setSearchQuery, isDisabled }: Props) => {
-  return (
-    <div className="relative mb-4">
-      <input
-        type="text"
-        placeholder="Zoek op ..."
-        disabled={isDisabled}
-        className="w-full p-3 border border-gray-300 rounded-md disabled:bg-gray-300"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <div className={"absolute inset-y-0 right-0 pr-3 flex items-center"}>
-        <SearchIcon />
+const SearchInput = memo(
+  ({ searchQuery, setSearchQuery, isDisabled }: Props) => {
+    return (
+      <div className="relative mb-4">
+        <input
+          type="text"
+          placeholder="Zoek op ..."
+          disabled={isDisabled}
+          className="w-full p-3 border border-gray-300 rounded-md disabled:bg-gray-300"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <div className={"absolute inset-y-0 right-0 pr-3 flex items-center"}>
+          <SearchIcon />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  },
+);
 
 export default SearchInput;
--- frontend/src/presentation/components/multi-select/ProductList.tsx
+++ frontend/src/presentation/components/multi-select/ProductList.tsx
@@ -1,7 +1,7 @@
 import { ProductType } from "../../../domain/types/productType";
 import ProductListItem from "./ProductListItem";
 import { FixedSizeList as List } from "react-window";
-import { CSSProperties, useMemo } from "react";
+import { memo, useMemo } from "react";
 import { usePlatform } from "../../hooks/usePlatform";
 
 interface Props {
@@ -10,80 +10,84 @@ interface Props {
   searchQuery: string;
 }
 
-export const ProductList = ({
-  productsList,
-  selectedProducts,
-  searchQuery,
-}: Props) => {
-  const { windowHeight, isMobile, isVerySmallDevice } = usePlatform();
+const ProductList = memo(
+  ({ productsList, selectedProducts, searchQuery }: Props) => {
+    const { windowHeight, isMobile, isVerySmallDevice } = usePlatform();
+    const filteredItems = useMemo(() => {
+      const filteredSelectedItems: ProductType[] = [];
+      const filteredUnselectedItems: ProductType[] = [];
 
-  const filteredItems = useMemo(() => {
-    const filteredSelectedItems: ProductType[] = [];
-    const filteredUnselectedItems: ProductType[] = [];
+      productsList.forEach((item) => {
+        if (selectedProducts.includes(item.id)) {
+          filteredSelectedItems.push(item);
+        } else if (
+          item.name.toLowerCase().includes(searchQuery.toLowerCase())
+        ) {
+          filteredUnselectedItems.push(item);
+        }
+      });
 
-    productsList.forEach((item) => {
-      if (selectedProducts.includes(item.id)) {
-        filteredSelectedItems.push(item);
-      } else if (item.name.toLowerCase().includes(searchQuery.toLowerCase())) {
-        filteredUnselectedItems.push(item);
+      return {
+        unselectedProducts: filteredUnselectedItems,
+        allProducts: [...filteredSelectedItems, ...filteredUnselectedItems],
+      };
+    }, [productsList, selectedProducts, searchQuery]);
+
+    const getItemSize = () => {
+      if (isVerySmallDevice) {
+        return 90;
       }
-    });
-
-    return {
-      unselectedList: filteredUnselectedItems,
-      products: [...filteredSelectedItems, ...filteredUnselectedItems],
+      if (isMobile) {
+        return 60;
+      }
+      return 50;
     };
-  }, [productsList, selectedProducts, searchQuery]);
 
-  const getItemSize = () => {
-    if (isVerySmallDevice) {
-      return 90;
-    }
-    if (isMobile) {
-      return 60;
-    }
-    return 50;
-  };
+    const getListHeight = () => {
+      const itemSize = getItemSize();
+      const itemCount = allProducts.length;
+      const calculatedHeight = itemCount * itemSize;
+      const minHeight = itemSize * 2;
+      const maxHeight = isMobile ? windowHeight - 400 : itemSize * 8;
 
-  const renderRow = ({
-    index,
-    style,
-  }: {
-    index: number;
-    style: CSSProperties;
-  }) => {
-    const product = products[index];
-    const isSelected = !!selectedProducts.find(
-      (productId) => productId === product.id,
-    );
+      return Math.max(minHeight, Math.min(calculatedHeight, maxHeight));
+    };
 
+    const { unselectedProducts, allProducts } = filteredItems;
     return (
-      <ProductListItem
-        isProductSelected={isSelected}
-        product={product}
-        listComponentStyle={style}
-        isVerySmallDevice={isVerySmallDevice}
-        isMobile={isMobile}
-      />
+      <div>
+        <List
+          height={getListHeight()}
+          itemCount={allProducts.length}
+          itemSize={getItemSize()}
+          width="100%"
+          style={{ overflowX: "hidden" }}
+        >
+          {({ index, style }) => (
+            <ProductListItem
+              isProductSelected={
+                !!selectedProducts.find(
+                  (productId) => productId === allProducts[index].id,
+                )
+              }
+              product={allProducts[index]}
+              listComponentStyle={style}
+              isVerySmallDevice={isVerySmallDevice}
+              isMobile={isMobile}
+            />
+          )}
+        </List>
+        {!unselectedProducts.length && (
+          <div>
+            <div className={"w-full h-0.5 my-4 bg-gray-400"} />
+            <div className="p-4 text-center text-red-500">
+              No item to be selected
+            </div>
+          </div>
+        )}
+      </div>
     );
-  };
+  },
+);
 
-  const { unselectedList, products } = filteredItems;
-  return (
-    <div>
-      <List
-        height={windowHeight - 400}
-        itemCount={products.length}
-        itemSize={getItemSize()}
-        width="100%"
-        style={{ overflowX: "hidden" }}
-        children={renderRow}
-      />
-      {!unselectedList.length && (
-        <div className="p-4 text-center text-red-500 flex-grow">
-          No item matches your search criteria!
-        </div>
-      )}
-    </div>
-  );
-};
+export default ProductList;
