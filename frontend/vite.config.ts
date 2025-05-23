d1b76ab29a2bd132e35a5beec73435c2e80c5189
Mehrad Alinezhad <iamehraad@gmail.com>
2025-05-23 18:48:20 +0200

    Updated tsconfig and vite config  for docker and port mapping

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,4 +7,8 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr(), dts()],
+  server: {
+    port: 3000,
+    host: true,
+  },
 });
