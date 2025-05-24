18b15eaf9d5fab207e6d2b690c543bee52f62c6a
Mehrad Alinezhad <iamehraad@gmail.com>
2025-05-24 10:37:49 +0200

    write complete test for common button component

--- frontend/src/presentation/components/common/common-button/CommonButton.test.tsx
+++ frontend/src/presentation/components/common/common-button/CommonButton.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import CommonButton from "./CommonButton";
 
-describe("User", () => {
-  test("renders heading", async () => {
+describe("Common button", () => {
+  test("renders correctly", async () => {
     render(
       <CommonButton onClick={() => {}} dataTestId={"jest"}>
         <span>Hi Bol</span>
@@ -10,5 +10,17 @@ describe("User", () => {
     );
     expect(screen.getByTestId("common-button-jest")).toBeInTheDocument();
     expect(screen.getByTestId("common-button-jest").querySelector('span')).toHaveTextContent("Hi Bol");
+  });
+
+  test("correctly triggers click handler", async () => {
+    const clickHandler = jest.fn();
+    render(
+        <CommonButton onClick={clickHandler} dataTestId={"jest"}>
+          <span>Hi Bol</span>
+        </CommonButton>,
+    );
+    const button = screen.getByTestId("common-button-jest");
+    fireEvent.click(button);
+    expect(clickHandler).toHaveBeenCalled();
   });
 });
