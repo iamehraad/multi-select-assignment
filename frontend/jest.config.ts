906163124c76e7204d89f08447554bdc742a0990
Mehrad Alinezhad <iamehraad@gmail.com>
2025-05-24 11:26:13 +0200

    Added search component tests

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,7 +1,11 @@
 export default {
     testEnvironment: "jsdom",
     transform: {
-        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    moduleNameMapper: {
+        '\\.svg\\?react$': '<rootDir>/__mocks__/svgReactMock.tsx',
+        '\\.svg$': '<rootDir>/__mocks__/fileMock.js',
     },
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
--- frontend/src/presentation/components/form/inputs/SearchInput.tsx
+++ frontend/src/presentation/components/form/inputs/SearchInput.tsx
@@ -1,14 +1,9 @@
 import SearchIcon from "../../icons/SearchIcon";
 import { memo } from "react";
-
-interface Props {
-  searchQuery: string;
-  isDisabled: boolean;
-  setSearchQuery: (query: string) => void;
-}
+import { SearchInputProps } from "../../../../domain/types/components/searchInputTypes";
 
 const SearchInput = memo(
-  ({ searchQuery, setSearchQuery, isDisabled }: Props) => {
+  ({ searchQuery, setSearchQuery, isDisabled }: SearchInputProps) => {
     return (
       <div className="relative mb-4">
         <input
@@ -16,10 +11,14 @@ const SearchInput = memo(
           placeholder="Zoek op ..."
           disabled={isDisabled}
           className="w-full p-3 border border-gray-300 rounded-md disabled:bg-gray-300"
+          data-testid="search-input"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <div className={"absolute inset-y-0 right-0 pr-3 flex items-center"}>
+        <div
+          className={"absolute inset-y-0 right-0 pr-3 flex items-center"}
+          data-testid="search-input-icon"
+        >
           <SearchIcon />
         </div>
       </div>
