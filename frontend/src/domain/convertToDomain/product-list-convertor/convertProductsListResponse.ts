99840fe0c16738383feadfd28851aa22231b529d
Mehrad Alinezhad <iamehraad@gmail.com>
2025-06-01 12:52:39 +0200

    removed redundant return from convertor

--- frontend/src/domain/convertToDomain/product-list-convertor/convertProductsListResponse.ts
+++ frontend/src/domain/convertToDomain/product-list-convertor/convertProductsListResponse.ts
@@ -1,12 +1,10 @@
-import type {ProductType} from "../../types/productType";
+import type { ProductType } from "../../types/productType";
 
 export const convertProductsListResponse = (
-  productsList: string[]
+  productsList: string[],
 ): ProductType[] => {
-    return productsList.map((product, index) => {
-      return {
-          id: product.replaceAll(" ", "") + index,
-          name: product,
-      };
-  });
+  return productsList.map((product, index) => ({
+    id: product.replaceAll(" ", "") + index,
+    name: product,
+  }));
 };
